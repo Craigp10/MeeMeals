f2e77ff00cf75bc166252098bf212a554d9d44c6
Craig <CMPeoples3@gmail.com>
2021-07-06 11:11:49 -0700

    Implemented static topbar component

--- client/src/components/TopBar/TopBar.js
+++ client/src/components/TopBar/TopBar.js
@@ -1,11 +1,16 @@
 import React from "react";
+import logout from "../../images/logout.png";
 import "./TopBar.css";
 
 const TopBar = (props) => (
   <div className="topbar-wrapper">
-    <div className="topbar-title">Mi Meals</div>
+    <div className="topbar-title">
+      <span className="topbar-title-text">Mi Meals</span>
+    </div>
     <div className="topbar-leftover">
-      <span className="topbar-icon">icon</span>
+      <span className="topbar-icon">
+        <img src={logout} />
+      </span>
     </div>
   </div>
 );
