db2beaccc00bbcaeced63ac147a9e42e842d32fd
Craig <CMPeoples3@gmail.com>
2021-07-07 14:09:12 -0700

    minor update to nav bar routing and styling

--- client/src/components/NavigationBar/NavigationBar.js
+++ client/src/components/NavigationBar/NavigationBar.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./NavigationBar.css";
+
 const navigationbar = (props) => (
   <div className="navigationbar-wrapper">
     <ul className="navigationbar-link-list">
       <React.Fragment>
         <li>
-          <Link className="navigationbar-link" to="/dashboard/">
+          <Link className="navigationbar-link" to="/">
             Home
           </Link>
         </li>
         <li>
-          <Link className="navigationbar-link" to="/dashboard/meals">
+          <Link className="navigationbar-link" to="/meals">
             Meals
           </Link>
         </li>
         <li>
-          <Link className="navigationbar-link" to="/dashboard/calendar">
+          <Link className="navigationbar-link" to="/calendar">
             Calendar
           </Link>
         </li>
@@ -24,12 +25,5 @@ const navigationbar = (props) => (
     </ul>
   </div>
 );
-
-// DashboardNav.propTypes = {
-//   navCollapsed: PropTypes.bool.isRequired,
-//   collapseNav: PropTypes.func.isRequired,
-//   pageSelected: PropTypes.string.isRequired,
-//   setPageName: PropTypes.func.isRequired,
-// };
 
 export default navigationbar;
