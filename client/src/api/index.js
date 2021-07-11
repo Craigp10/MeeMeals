3f121dd090526f893919c12325f1781ba4fcaa76
Craig <CMPeoples3@gmail.com>
2021-07-11 13:40:47 -0700

    Revert "Updating frontend api pathing"
    This reverts commit 61c5cb330a39d7b28aab024a2df31ebfa4190692.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,13 +4,19 @@ const api = axios.create({
   baseURL: "http://localhost:3000/api",
 });
 
-export const getAllMeals = () => api.get(`/meals`);
-
-// export const getDatemeals = () => api.post(`/user/calendar/`);
+export const insertMovie = (payload) => api.post(`/movie`, payload);
+export const getAllMovies = () => api.get(`/movies`);
+export const updateMovieById = (id, payload) =>
+  api.put(`/movie/${id}`, payload);
+export const deleteMovieById = (id) => api.delete(`/movie/${id}`);
+export const getMovieById = (id) => api.get(`/movie/${id}`);
 
 const apis = {
-  getAllMeals,
-  // getDateMeals,
+  insertMovie,
+  getAllMovies,
+  updateMovieById,
+  deleteMovieById,
+  getMovieById,
 };
 
 export default apis;
