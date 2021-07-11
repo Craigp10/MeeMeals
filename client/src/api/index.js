61c5cb330a39d7b28aab024a2df31ebfa4190692
Craig <CMPeoples3@gmail.com>
2021-07-11 13:40:12 -0700

    Updating frontend api pathing

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,19 +4,13 @@ const api = axios.create({
   baseURL: "http://localhost:3000/api",
 });
 
-export const insertMovie = (payload) => api.post(`/movie`, payload);
-export const getAllMovies = () => api.get(`/movies`);
-export const updateMovieById = (id, payload) =>
-  api.put(`/movie/${id}`, payload);
-export const deleteMovieById = (id) => api.delete(`/movie/${id}`);
-export const getMovieById = (id) => api.get(`/movie/${id}`);
+export const getAllMeals = () => api.get(`/meals`);
+
+// export const getDatemeals = () => api.post(`/user/calendar/`);
 
 const apis = {
-  insertMovie,
-  getAllMovies,
-  updateMovieById,
-  deleteMovieById,
-  getMovieById,
+  getAllMeals,
+  // getDateMeals,
 };
 
 export default apis;
