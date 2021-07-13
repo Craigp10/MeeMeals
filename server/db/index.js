1135d28935cf09aea060271c0b0b4633afbdabad
Craig <CMPeoples3@gmail.com>
2021-07-12 19:44:06 -0700

    Updating initial data insert with updated User collection

--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,7 @@ const db_schema = require("../models/index");
 const dbConfig = require("../config/db.config");
 const Role = db_schema.role;
 const Meals = db_schema.meals;
+const User = db_schema.user;
 
 mongoose
   .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
@@ -53,6 +54,82 @@ function initial() {
       });
     }
   });
+
+  User.find({ username: "Craigp10" }, (err, user) => {
+    if (!user[0].meals.length) {
+      const new_meals = [
+        {
+          display_name: "Spinach Feta pasta w/ cherry tomatoes",
+          ingredients: [
+            "Cherry Tomatoes",
+            "Spinach",
+            "pasta of choice",
+            "onion",
+          ],
+          date_created: "7/6/2021",
+          category: "dinner",
+          description:
+            "First seer the tomatoes with a hot pan, once seered for color lower heat and add oil. Add the onions and garlic, once those are fraguent then everything else. After bake for 30mins.",
+        },
+        {
+          display_name: "Sausage, Rice, Peppers dish",
+          ingredients: ["Sausage", "Rice", "Bell Peppers", "Yellow Onion"],
+          date_created: "7/10/2021",
+          category: "lunch",
+          description: "",
+        },
+        {
+          display_name: "Cereal",
+          ingredients: ["almost milk", "cinnamon toast crunch"],
+          date_created: "7/1/2021",
+          category: "breakfast",
+          description: "Pour milk then pour in cereal",
+        },
+        {
+          display_name: "Chicken Parm",
+          ingredients: [
+            "Chicken breast",
+            "basil",
+            "cheese",
+            "breadcrumbs",
+            "egg",
+            "pasta",
+          ],
+          date_created: "12/6/2020",
+          category: "dinner",
+          description:
+            "Bread the chicken breast, fry them, make your sauce and pasta, bake everything at 350 for 30 minutes",
+        },
+        {
+          display_name: "Pad Thai",
+          ingredients: ["Peanut Butter", "egg", "pad thai sauce", "onion"],
+          date_created: "5/6/2019",
+          category: "dinner",
+          description: "",
+        },
+        {
+          display_name: "Peanut Butter Toast",
+          ingredients: ["bread", "creamy peanut butter"],
+          date_created: "5/6/2019",
+          category: "snack",
+          description: "",
+        },
+      ];
+      User.findOneAndUpdate(
+        { _id: user[0]._id },
+        { $set: { meals: new_meals } },
+        { new: true },
+        (err, doc) => {
+          if (err) {
+            console.log("Something wrong when updating data!");
+          }
+
+          console.log("Successfully updated user", doc);
+        }
+      );
+    }
+  });
+
   Meals.find({}, (err, meals) => {
     if (!meals.length) {
       [
@@ -125,9 +202,6 @@ function initial() {
       });
     }
   });
-  // console.log("existing_meals", existing_meals.length);
-  // if (existing_meals.length == 0) {
-  // }
 }
 
 db = mongoose.connection;
