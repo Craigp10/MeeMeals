3825495dd04326d3070e00abffc3e81628a50fa7
Craig <CMPeoples3@gmail.com>
2021-08-09 16:42:53 -0700

    minor updates to pages and user check before rendering

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -19,43 +19,52 @@ const Dashboard = (props) => {
   const history = useHistory();
 
   useEffect(() => {
+    //When props change setUser from props
     if (props?.user) {
       setUser(props.user);
     }
   }, [props]);
 
   const logout = () => {
-    console.log("logout clicked");
+    //Logs user out
     apis.logout().then((resp) => {
       console.log(resp);
       props.setAuthenticated(false);
       history.push("/");
     });
   };
+
   return (
     <div className="dashboard-wrapper">
-      <Router>
-        <div className="dashboard__content">
-          <div className="dashboard__content-topbar">
-            <TopBar logout={logout} />
+      {Object.keys(user).length ? (
+        <Router>
+          <div className="dashboard__content">
+            <div className="dashboard__content-topbar">
+              <TopBar logout={logout} />
+            </div>
+            <div className="dashboard__content-navigationbar">
+              <NavigationBar />
+            </div>
+            <div className="dashboard__content-inner">
+              <Switch>
+                <Route exact path="/" render={() => <Home user={user} />} />
+                <Route
+                  exact
+                  path="/meals"
+                  render={() => <Meals user={user} />}
+                />
+                <Route
+                  exact
+                  path="/calendar"
+                  render={() => <Calendar user={user} />}
+                />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
           </div>
-          <div className="dashboard__content-navigationbar">
-            <NavigationBar />
-          </div>
-          <div className="dashboard__content-inner">
-            <Switch>
-              <Route exact path="/" render={() => <Home user={user} />} />
-              <Route exact path="/meals" render={() => <Meals user={user} />} />
-              <Route
-                exact
-                path="/calendar"
-                render={() => <Calendar user={user} />}
-              />
-              <Route component={NotFound} />
-            </Switch>
-          </div>
-        </div>
-      </Router>
+        </Router>
+      ) : // Could add loading component
+      null}
     </div>
   );
 };
--- client/src/pages/Meals/Meals.js
+++ client/src/pages/Meals/Meals.js
@@ -10,18 +10,23 @@ const Meals = (props) => {
   const [modalAction, setModalAction] = useState("");
   const [activeMeal, setActiveMeal] = useState({});
   const [searchFilter, setSearchFilter] = useState("");
+
   useEffect(async () => {
-    await apis
-      .getUserMeals({ user_id: props.user.id })
-      .then((resp) => setMeals(resp.data.meals));
-  }, [props]);
+    //When props change pull user meals by user id
+    await apis.getUserMeals({ user_id: props.user.id }).then((resp) => {
+      console.log(resp.data.meals);
+      setMeals(resp.data.meals);
+    });
+  }, []);
 
   const handleClose = () => {
+    //close modal, remove active meal state
     setActiveMeal({});
     setShow(false);
   };
 
   const handleShow = (action, idx) => {
+    //show modal, set activeMeal if preview or edit action
     if (action == "edit" || action == "preview") {
       setActiveMeal({ ...meals[idx] });
     }
@@ -30,6 +35,7 @@ const Meals = (props) => {
   };
 
   const handleSubmit = async (data) => {
+    //Submit modal data
     console.log("submitting data", data);
     const requestObj = {
       meal: data,
@@ -55,6 +61,7 @@ const Meals = (props) => {
   };
 
   const handleDelete = async (meal_id) => {
+    //handle deleting a meal on click
     const requestObj = {
       meal_id,
       user_id: props.user.id,
@@ -65,13 +72,6 @@ const Meals = (props) => {
     });
   };
 
-  // if (props.location.state?.mealPreview && !show) {
-  //   console.log("MEAL PREVIEW1", props.location.state.mealPreview);
-  //   handleShow(
-  //     "preview",
-  //     meals.filter((meal) => meal._id == props.location.state.mealPreview)[0]
-  //   );
-  // }
   return (
     <div className="meals-wrapper">
       {show ? (
@@ -86,7 +86,7 @@ const Meals = (props) => {
       <div className="meals__board">
         <div className="meals__board__header">
           <div className="meals__board__header-search">
-            <span className="meals__board__header-search glyphicon glyphicon-search">
+            <div className="glyphicon glyphicon-search">
               <input
                 id="search"
                 value={props.searchFilter}
@@ -94,7 +94,7 @@ const Meals = (props) => {
                 onChange={(e) => setSearchFilter(e.target.value)}
                 autoComplete={"off"}
               />
-            </span>
+            </div>
           </div>
           <div className="meals__board__header-title">Your Meals</div>
           <div className="meals__board__header-create">
