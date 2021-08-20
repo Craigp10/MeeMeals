5632041efd0387732f86dafd1832377977e802aa
Craig <CMPeoples3@gmail.com>
2021-08-20 16:14:59 -0700

    Implementing typescript with all basic components

--- client/src/components/TopBar/TopBar.tsx
+++ client/src/components/TopBar/TopBar.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import logout from "../../images/logout.png";
 import "./TopBar.css";
 
-const TopBar = (props) => (
+type Props = {
+  logout: () => void;
+}
+
+const TopBar = (props:Props) => (
   <div className="topbar-wrapper">
     <div className="topbar__title">
       <span
         className="topbar__title-text"
-        onClick={() => (window.location = window.location.origin)}
+        onClick={() => (window.location.href = window.location.origin)}
       >
         MeeMeals
       </span>
@@ -16,7 +20,6 @@ const TopBar = (props) => (
       <div className="topbar__leftover-icon">
         <span
           className="glyphicon glyphicon-user"
-          alt="logout"
           onClick={props.logout}
         ></span>
       </div>
