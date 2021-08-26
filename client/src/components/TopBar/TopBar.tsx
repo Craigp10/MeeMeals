5263058c8d2c470d860b533d3e9b99e65c82b5a6
Craig <CMPeoples3@gmail.com>
2021-08-26 16:29:28 -0700

    minor topbar and dashboard changes, changes are commented out

--- client/src/components/TopBar/TopBar.tsx
+++ client/src/components/TopBar/TopBar.tsx
@@ -2,29 +2,40 @@ import React from "react";
 import logout from "../../images/logout.png";
 import "./TopBar.css";
 
+interface User {
+  id: string;
+  username: string;
+  email: string;
+}
+
 type Props = {
   logout: () => void;
+  user: User;
 };
 
-const TopBar = (props: Props) => (
-  <div className="topbar-wrapper">
-    <div className="topbar__title">
-      <span
-        className="topbar__title-text"
-        onClick={() => (window.location.href = window.location.origin)}
-      >
-        MeeMeals
-      </span>
-    </div>
-    <div className="topbar__leftover">
-      <div className="topbar__leftover-icon">
+const TopBar = (props: Props) => {
+  console.log("topbar", props.user.username);
+  return (
+    <div className="topbar-wrapper">
+      <div className="topbar__title">
         <span
-          className="glyphicon glyphicon-user"
-          onClick={props.logout}
-        ></span>
+          className="topbar__title-text"
+          onClick={() => (window.location.href = window.location.origin)}
+        >
+          MeeMeals
+        </span>
+      </div>
+      {/* <div className="topbar__user">Hello, {props.user.username}</div> */}
+      <div className="topbar__leftover">
+        <div className="topbar__leftover-icon">
+          <span
+            className="glyphicon glyphicon-user"
+            onClick={props.logout}
+          ></span>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default TopBar;
