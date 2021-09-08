5b1c354edcc309b5a58a7ce44287edd64c326c87
Craig <CMPeoples3@gmail.com>
2021-09-07 21:56:07 -0700

    actual NavBar updates

--- client/src/components/NavigationBar/NavigationBar.tsx
+++ client/src/components/NavigationBar/NavigationBar.tsx
@@ -9,56 +9,39 @@ const NavigationBar = (props: any) => {
   const hideNavBar = () => {
     props.setShow(false);
   };
-
   return (
-    <div className="navigationbar-wrapper">
-      {windowSize.width <= 768 ? (
-        <div className="navigationbar-mobile">
-          <span
-            className="glyphicon glyphicon-remove"
-            onClick={hideNavBar}
-          ></span>
-          <ul className="navigationbar__linklist">
-            <React.Fragment>
-              <li>
-                <Link className="navigationbar__linklist-link" to="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link className="navigationbar__linklist-link" to="/meals">
-                  Meals
-                </Link>
-              </li>
-              <li>
-                <Link className="navigationbar__linklist-link" to="/calendar">
-                  Calendar
-                </Link>
-              </li>
-            </React.Fragment>
-          </ul>
-        </div>
-      ) : (
-        <ul className="navigationbar__linklist">
-          <React.Fragment>
-            <li>
-              <Link className="navigationbar__linklist-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link className="navigationbar__linklist-link" to="/meals">
-                Meals
-              </Link>
-            </li>
-            <li>
-              <Link className="navigationbar__linklist-link" to="/calendar">
-                Calendar
-              </Link>
-            </li>
-          </React.Fragment>
-        </ul>
-      )}
+    <div
+      className={
+        windowSize.width <= 768 && props.show
+          ? "navigationbar-wrapper shown"
+          : "navigationbar-wrapper notShown"
+      }
+    >
+      <div className="navigationbar-mobile">
+        <span
+          className="glyphicon glyphicon-remove"
+          onClick={hideNavBar}
+        ></span>
+      </div>
+      <ul className="navigationbar__linklist" onClick={hideNavBar}>
+        <React.Fragment>
+          <li>
+            <Link className="navigationbar__linklist-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="navigationbar__linklist-link" to="/meals">
+              Meals
+            </Link>
+          </li>
+          <li>
+            <Link className="navigationbar__linklist-link" to="/calendar">
+              Calendar
+            </Link>
+          </li>
+        </React.Fragment>
+      </ul>
     </div>
   );
 };
